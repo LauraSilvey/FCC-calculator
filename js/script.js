5bc340188118edc000a6e692d8625bad458d4ecb
Laura Silvey <lsilvey@gmail.com>
2016-09-13 18:39:34 -0700

    construct basic calc layout and functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
 
+
+$(document).ready(function(){
+  var number = "";	
+  var firstNumber = "";
+  var operator = "";
+  var display = $("#total");
+  var counter = 0;
+
+  function reset() {
+  	firstNumber = display.text();
+    number = "";  	
+    operator = "";
+    counter ++;
+  }
+
+  $("#buttons #number").click(function(){
+    number += $(this).text();
+    display.text(number);
+  });
+
+  $("#buttons #operation").click(function(){
+    operator = $(this).text();
+    if(counter < 1){
+      firstNumber = number;
+      number = "";
+    }
+
+  });
+
+  $("#buttons #equals").click(function(){
+    if(operator === "+"){
+    	display.text(parseInt(firstNumber) + parseInt(number));
+        reset();
+    }else if(operator === "-"){
+        display.text(parseInt(firstNumber) - parseInt(number));
+        reset();
+    }else if(operator === "/"){
+    	display.text(parseInt(firstNumber) / parseInt(number));
+    	reset();
+    }else if(operator === "*"){
+    	display.text(parseInt(firstNumber) * parseInt(number));
+    	reset();
+    }
+
+  });
+
+});
+
+
+  
+
+    
+
+
+
+
+
