3b2951276f046a0827cf73d23b6095cee9a28aaf
Laura Silvey <lsilvey@gmail.com>
2016-09-17 11:32:06 -0700

    Added functionality to decimal and percent buttons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-
-
 $(document).ready(function(){
   var number = "";	
   var firstNumber = "";
   var operator = "";
   var display = $("#total");
   var counter = 0;
+
+  function checkLength(number){
+    if(number.length > 6){
+      display.text(number.substr(number.length - 6, 6));
+    }  
+  }
 
   function reset() {
   	firstNumber = display.text();
@@ -14,37 +18,78 @@ $(document).ready(function(){
     counter ++;
   }
 
+  function total(){
+    if(operator === "+"){
+      number = parseFloat(firstNumber) + parseFloat(number);
+    }else if(operator === "-"){
+      number = parseInt(firstNumber) - parseInt(number); 
+    }else if(operator === "/"){
+      number = parseInt(firstNumber) / parseInt(number);  
+    }else if(operator === "*"){
+      number = parseInt(firstNumber) * parseInt(number);
+    }
+    display.text(number);
+    if(display.text().length > 6){
+      display.text("Err");
+    }
+    reset();
+  }
+
   $("#buttons #number").click(function(){
     number += $(this).text();
     display.text(number);
+    checkLength(number);
   });
 
   $("#buttons #operation").click(function(){
-    operator = $(this).text();
-    if(counter < 1){
-      firstNumber = number;
-      number = "";
+    if(operator === ""){
+      operator = $(this).text();
+      if(counter < 1){
+        firstNumber = number;
+        number = "";
+      }
+    }else if(number === ""){
+      operator = $(this).text();
+    }else{
+      total();
+      operator = $(this).text();
+      firstNumber = display.text();    
     }
-
   });
 
   $("#buttons #equals").click(function(){
-    if(operator === "+"){
-    	display.text(parseInt(firstNumber) + parseInt(number));
-        reset();
-    }else if(operator === "-"){
-        display.text(parseInt(firstNumber) - parseInt(number));
-        reset();
-    }else if(operator === "/"){
-    	display.text(parseInt(firstNumber) / parseInt(number));
-    	reset();
-    }else if(operator === "*"){
-    	display.text(parseInt(firstNumber) * parseInt(number));
-    	reset();
-    }
-
+    if(firstNumber !== "" && number !== ""){
+      total();
+    }  
   });
 
+  $("#buttons #clearall").click(function(){
+    display.text("0");
+    firstNumber = "";
+    number = "";
+    operator = "";
+    counter = 0;
+  });
+
+  $("#buttons #clear").click(function(){
+    number = "";
+    display.text("0");
+  });
+
+  $("#buttons #percent").click(function(){
+    if(number !== ""){
+      number = parseInt(number) / 100;
+      display.text(number);
+    }
+  });
+    
+  $("#buttons #decimal").click(function(){
+    if(!number.includes('.')){
+      number += $(this).text();
+      display.text(number);  
+      checkLength(number);
+    }      
+  });  
 });
 
 
